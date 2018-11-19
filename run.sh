4e9657d78dcc49b66472f42a67a3f9ef3213ac83
Alber Erre <arblugo@gmail.com>
2018-11-19 18:18:16 +0000

    update route

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -e MYSQL_ROOT_PASSWORD=1234 -d -p 3333:3306 -v /Users/db/mariadb:/var/lib/mysql mariadb
+docker run -e MYSQL_ROOT_PASSWORD=1234 -d -p 3333:3306 -v /Users/mariadb:/var/lib/mysql mariadb
