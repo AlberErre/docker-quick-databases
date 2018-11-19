ea394e1aac77090ed34cb436194ae1d45611c745
Alber Erre <arblugo@gmail.com>
2018-11-19 18:01:40 +0000

    mac OS init local store

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -e MYSQL_ROOT_PASSWORD=1234 -d -p 3333:3306 -v /your/local/store/path:/var/lib/mysql mariadb
+docker run -e MYSQL_ROOT_PASSWORD=1234 -d -p 3333:3306 -v /Users/db/mariadb:/var/lib/mysql mariadb
